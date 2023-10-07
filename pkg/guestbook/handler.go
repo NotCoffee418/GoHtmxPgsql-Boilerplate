a752f97259d6f84c6287a4d07e30918464b2f401
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2023-10-07 20:09:58 +0200

    Work on guestbook (WIP)

--- pkg/guestbook/handler.go
+++ pkg/guestbook/handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/NotCoffee418/GoWebsite-Boilerplate/internal/config"
 	"github.com/NotCoffee418/GoWebsite-Boilerplate/internal/types"
 	"github.com/NotCoffee418/GoWebsite-Boilerplate/internal/weberrors"
+	"github.com/NotCoffee418/GoWebsite-Boilerplate/pkg/validation"
 	"github.com/NotCoffee418/websocketmanager"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -33,54 +34,84 @@ func (h *Handler) Initialize(initContext *types.HandlerInitContext) {
 
 // GuestbookHandler Handler Implements RouteRegistrar interface
 func (h *Handler) GuestbookHandler(engine *gin.Engine) {
-	engine.GET("/home/guestbook/ws", h.guestbookWS)
+	engine.GET("/home/guestbook/ws", h.guestbookWsInvoker)
 	engine.POST("/home/guestbook/submit", h.guestbookPost)
 	engine.GET("/home/guestbook/recent", h.getRecentGuestbookEntries)
 }
 
-func (h *Handler) guestbookWS(context *gin.Context) {
-	<-wsManager.UpgradeClientCh(context.Writer, context.Request)
+func (h *Handler) guestbookWsInvoker(context *gin.Context) {
+	client := <-wsManager.UpgradeClientCh(context.Writer, context.Request)
+	wsManager.RegisterClientObserver(*client.ConnId, guestbookWsHandler)
 }
 
-func (h *Handler) guestbookPost(context *gin.Context) {
-	// Get form data
-	name := context.PostForm("name")
-	message := context.PostForm("message")
-
-	// Validation logic
-	if len(name) == 0 {
-		context.String(http.StatusBadRequest, "Name cannot be empty")
-		return
-	}
-	if len(name) > 255 {
-		context.String(http.StatusBadRequest, "Name cannot be longer than 50 characters")
-		return
-	}
-	if len(message) == 0 {
-		context.String(http.StatusBadRequest, "Message cannot be empty")
-		return
-	}
-
-	// Insert into database
-	err := gbRepo.Insert(db, name, message)
-	if err != nil {
-		context.String(http.StatusInternalServerError, "Error inserting into database")
-		return
-	}
-
-	// Broadcast update to all clients
-	h.triggerGuestbookUpdate()
-}
-
-func (h *Handler) triggerGuestbookUpdate() {
+func guestbookWsHandler(wsClient *websocketmanager.Client, messageType int, message []byte) {
 	wsManager.BroadcastMessage(websocket.TextMessage, []byte("New guestbook entry"))
 }
 
+func (h *Handler) guestbookPost(c *gin.Context) {
+	// Get form data
+	name := c.PostForm("name")
+	message := c.PostForm("message")
+
+	success := true
+	var respMsgs []string
+
+	// Validation logic
+	if len(name) == 0 {
+		respMsgs = append(respMsgs, "Name cannot be empty")
+		success = false
+	}
+	if len(name) > 255 {
+		respMsgs = append(respMsgs, "Name cannot be longer than 50 characters")
+		success = false
+	}
+	if len(message) < 4 {
+		respMsgs = append(respMsgs, "Message cannot be empty")
+		success = false
+	}
+
+	// Insert into database
+	if success {
+		err := gbRepo.Insert(db, name, message)
+		if err != nil {
+			respMsgs = append(respMsgs, "Internal server error")
+			success = false
+		}
+	}
+
+	if success {
+		// All is well, add success message
+		respMsgs = append(respMsgs, "Successfully added post")
+
+		// Broadcast update to all clients
+		h.triggerGuestbookUpdate()
+	}
+
+	// Render form response
+	var respData []validation.FormResponse
+	for _, msg := range respMsgs {
+		respData = append(respData, validation.FormResponse{
+			IsSuccessful: success,
+			Message:      msg,
+		})
+	}
+	validation.RenderFormResponse(c, respData)
+}
+
+func (h *Handler) triggerGuestbookUpdate() {
+	go wsManager.BroadcastMessage(websocket.TextMessage, []byte("New guestbook entry"))
+}
+
 func (h *Handler) getRecentGuestbookEntries(c *gin.Context) {
-	recent, err := gbRepo.GetRecent(db, 10)
+	recentPosts, err := gbRepo.GetRecent(db, 10)
 	if err != nil {
 		weberrors.InternalServerErrorResponse(c, err)
 		return
 	}
 
+	data := &map[string]interface{}{
+		"Posts": recentPosts,
+	}
+
+	c.HTML(http.StatusOK, "guestbook_posts.html", data)
 }
